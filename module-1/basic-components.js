3642ed4cd2d35602cf0987bffaca308c872bb888
Kawsar <web.kawsarahed@gmail.com>
2024-01-10 11:01:30 +0600

    added react practice coe and concepts

---
+++ module-1/basic-components.js
@@ -0,0 +1,14 @@
+/** 
+what is component?
+=> 
+
+
+components ki return kore?
+=> কম্পোনেন্ট এর রিটার্ন এ সব সময় একটি অবজেক্ট রিটার্ণ করতে হবে
+=> কম্পোনেন্ট যদি অন্য কোথাও ব্যবহার করতে চাই তবে সেটি Export করতে হবে
+
+comonent theke ki return hoy?
+=> jsx return hoy not HTML
+
+
+*/
